f5fff65ed7bd13747052e89854b5e4c0b7523966
Drashya Raghav <drashyaraghav@gmail.com>
2026-06-11 16:54:54 +0530

    Add files via upload

---
+++ code/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("STARTING THE APP......");
+        boolean log = false;
+
+       
+     
+        System.out.println("Logging In");
+        WelcomePage wp = new WelcomePage(log);
+    }
+}
